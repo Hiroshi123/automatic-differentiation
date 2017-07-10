4219c454646cba7fb4869f198d1652fe84c3d3b6
Hiroshi123 <vrodxda@gmail.com>
2017-07-10 23:36:45 +0900

    init

---
+++ src/calculator.cpp
@@ -0,0 +1,41 @@
+
+
+
+//#include "symbol.hpp"
+//#include "node.hpp"
+//#include "tree.hpp"
+#include "calc.hpp"
+
+//enum {a = 5, b = 3 , c = 4};
+
+int main(){
+  
+  Calc calc(std::cin);
+  
+  calc.tentativeLoop();
+  
+  //calc.main_loop();
+  
+  //char* a = new char[4];
+  
+  //calc.expr();
+  
+  //calc.count();
+  
+  //char* a = new char[3];
+  //a[0] = 4;
+  //std::cout << a << std::endl;
+  
+  
+  //calc.get();
+  
+  //char d = static_cast<char>(Token::plus);
+  
+  //int s  = static_cast<int>(Token::number);
+  
+  //std::cout << d << std::endl;
+  //std::cout << a << std::endl;
+  
+};
+
+
