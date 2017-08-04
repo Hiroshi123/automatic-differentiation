d60bc172f8d41fcd7c184ff161e7b0633ee90020
Hiroshi123 <vrodxda@gmail.com>
2017-08-04 16:16:04 +0900

    [edit] syntax

--- else/Syntax.hs
+++ else/Syntax.hs
@@ -1,7 +1,5 @@
 
-
 module Syntax where
-
 
 data S = S Struct
        | F Func
@@ -11,14 +9,14 @@ data S = S Struct
 ---------------------------------------------------
 ------------ non-terminal item definition --------------
 
-
 data Struct = Struct Lb S Rb Sem  deriving (Show)
-data Func   = Func Ty FName Args Lb S Rb Sem  deriving (Show)
-data Decl   = Decl Lb S Rb   Sem  deriving (Show)
+data Func   = Func Static Const Ty FName Args Lb S Rb Sem  deriving (Show)
+data Decl   = Decl Static Const VName Sem  deriving (Show)
 
 data FName = FName [Char] deriving (Show)
 data Args  = Args Lp Arg Rp  deriving (Show)
-
+data VName = VName [Char] deriving (Show)
+  
 ---------------------------------------------------
 
 data Ty  = Int | Float | Double | Void  deriving (Show)
@@ -36,15 +34,13 @@ data Sem = Sem Char  deriving (Show)
 ---------------------------------------------------
 ------------ terminal item definition -----------
 
-
 type Arg = [(Ty,[Char])]
 
+data Static = Static Bool deriving (Show)
+data Const  = Const  Bool deriving (Show)
 
 
---type Ty = Int 
-
+--type Ty = Int
 -- Struct = Struct (Lb 'x') (Rb 'x')
-
-
 -- "Struct { int a = 3;};"
 
